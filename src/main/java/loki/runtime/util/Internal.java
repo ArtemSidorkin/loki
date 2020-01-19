b527f5418de4632492a66c32c6845bf6485f1864
Artem Sidorkin <ArtemSidorkin1@gmail.com>
2020-01-19 17:49:28 +0300

    Added missed meta annotations.

---
+++ src/main/java/loki/runtime/util/Internal.java
@@ -0,0 +1,10 @@
+package loki.runtime.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface Internal {}
