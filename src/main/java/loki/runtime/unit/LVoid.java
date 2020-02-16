3e727f75ecb30323667a64d0eaca1337ed25858a
Artem Sidorkin <ArtemSidorkin1@gmail.com>
2020-02-16 20:06:58 +0300

    Improved void.

--- src/main/java/loki/runtime/unit/LVoid.java
+++ src/main/java/loki/runtime/unit/LVoid.java
@@ -1,6 +1,8 @@
 package loki.runtime.unit;
 
+import loki.runtime.constant.LBinaryOperator;
 import loki.runtime.constant.LDataUnit;
+import loki.runtime.unit.bool.LBoolean;
 import loki.runtime.unit.type.LType;
 import loki.runtime.unit.unit.LUnit;
 import loki.runtime.util.LErrors;
@@ -17,9 +19,17 @@ public class LVoid extends LUnit
 	}
 
 	@Override
-	public LUnit addParents(LUnit... parents)
+	public LUnit newInstance(LUnit[] parameters, Consumer<LUnit> saver)
 	{
-		LErrors.printErrorAndExit("Adding parents to \"void\" is not allowed");
+		LErrors.printErrorAndExit("Instantiating \"void\" is not allowed");
+
+		return null;
+	}
+
+	@Override
+	public LUnit getMember(String memberName)
+	{
+		LErrors.printErrorAndExit("Getting member of \"void\"  is not allowed");
 
 		return null;
 	}
@@ -33,9 +43,17 @@ public class LVoid extends LUnit
 	}
 
 	@Override
-	public LUnit newInstance(LUnit[] parameters, Consumer<LUnit> saver)
+	public LUnit getSuperMember(String superMemberName)
 	{
-		LErrors.printErrorAndExit("Instantiating \"void\" is not allowed");
+		LErrors.printErrorAndExit("Getting super member of \"void\"  is not allowed");
+
+		return null;
+	}
+
+	@Override
+	public LUnit addParents(LUnit... parents)
+	{
+		LErrors.printErrorAndExit("Adding parents to \"void\" is not allowed");
 
 		return null;
 	}
@@ -46,5 +64,45 @@ public class LVoid extends LUnit
 		LErrors.printErrorAndExit("Calling \"void\" is not allowed");
 
 		return null;
+	}
+
+	@Override
+	public LUnit callMember(String memberName, LUnit[] parameters)
+	{
+		if (LBinaryOperator.EQUALS_EQUALS.symbol.equals(memberName))
+			return LBoolean.valueOf(this == checkCallParameter(parameters, 0));
+
+		if (LBinaryOperator.BANG_EQUALS.symbol.equals(memberName))
+			return LBoolean.valueOf(this != checkCallParameter(parameters, 0));
+
+		LErrors
+			.printErrorAndExit(
+				String
+					.format(
+						"Calling member of \"void\" is not allowed except \"%s\" and \"%s\"",
+						LBinaryOperator.EQUALS_EQUALS,
+						LBinaryOperator.BANG_EQUALS
+					)
+			);
+
+		return null;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object object)
+	{
+		return INSTANCE == object;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "void";
 	}
 }
