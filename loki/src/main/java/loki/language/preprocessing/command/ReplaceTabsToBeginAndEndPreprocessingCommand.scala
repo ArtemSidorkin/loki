191f3c463ad6e5b4557fddf98e9c48282ee16cc0
Artem Sidorkin <ArtemSidorkin1@gmail.com>
2020-04-05 21:48:12 +0300

    Improved brace inference.

--- loki/src/main/java/loki/language/preprocessing/command/ReplaceTabsToBeginAndEndPreprocessingCommand.scala
+++ loki/src/main/java/loki/language/preprocessing/command/ReplaceTabsToBeginAndEndPreprocessingCommand.scala
@@ -2,34 +2,57 @@ package loki.language.preprocessing.command
 
 import loki.language.preprocessing.constant.CompilerTokens
 
+import scala.collection.mutable
 import scala.language.postfixOps
 
 private[preprocessing] object ReplaceTabsToBeginAndEndPreprocessingCommand extends PreprocessingCommand
 {
 	override def apply(code:StringBuilder):Unit =
 	{
-		val codeLines = code.toString split CompilerTokens.NEW_LINE
+		val tabStack = mutable.Stack[Int]()
+
+		val codeLines = code.toString.split(CompilerTokens.NEW_LINE)
 		val newCode = new StringBuilder
 
-		val lastCodeLineTabCount =
-			codeLines.foldLeft(0)((previousLineTabCount, codeLine) =>
+		val lastCodeLine =
+			codeLines.foldLeft("")((previousLine, codeLine) =>
 			{
+				val previousLineTabCount = countTabsInCodeLine(previousLine)
+
 				val tabCountInCurrentLine = countTabsInCodeLine(codeLine)
 				val lineWithoutTab = codeLine filter CompilerTokens.TAB.!=
 
 				if (tabCountInCurrentLine > previousLineTabCount)
-					newCode ++=
-						s"${CompilerTokens.NEW_LINE}${CompilerTokens.LEFT_BRACE}" *
-						(tabCountInCurrentLine - previousLineTabCount)
+				{
+					if (previousLine.endsWith(":") || previousLine.endsWith("else") || previousLine.endsWith("\\"))
+					{
+						for (_ <- 0 until tabCountInCurrentLine - previousLineTabCount) tabStack.push(1)
+
+						newCode ++=
+							s"${CompilerTokens.NEW_LINE}${CompilerTokens.LEFT_BRACE}" *
+								(tabCountInCurrentLine - previousLineTabCount)
+					}
+					else
+					{
+						for (_ <- 0 until tabCountInCurrentLine - previousLineTabCount) tabStack.push(0)
+					}
+				}
+
 				else if (tabCountInCurrentLine < previousLineTabCount)
-					newCode ++=
-						s"${CompilerTokens.NEW_LINE}${CompilerTokens.RIGHT_BRACE}" *
-						(previousLineTabCount - tabCountInCurrentLine)
+				{
+					for (_ <- 0 until previousLineTabCount - tabCountInCurrentLine)
+					{
+						if (tabStack.pop() == 1) newCode ++= s"${CompilerTokens.NEW_LINE}${CompilerTokens.RIGHT_BRACE}"
+					}
+				}
 
 				if (newCode.nonEmpty) newCode += CompilerTokens.NEW_LINE
 				newCode ++= lineWithoutTab
-				tabCountInCurrentLine
+
+				codeLine
 			})
+
+		val lastCodeLineTabCount = countTabsInCodeLine(lastCodeLine)
 
 		newCode ++= s"${CompilerTokens.NEW_LINE}${CompilerTokens.RIGHT_BRACE}" * lastCodeLineTabCount
 		code.clear()
