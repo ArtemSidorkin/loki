21863d9f466ec743ad33615e49e45cfb5ed55f1a
Artem Sidorkin <ArtemSidorkin1@gmail.com>
2020-04-08 23:01:12 +0300

    Partially refactored preprocessing.

--- loki/src/main/java/loki/language/preprocessing/Preprocessor.scala
+++ loki/src/main/java/loki/language/preprocessing/Preprocessor.scala
@@ -9,7 +9,7 @@ object Preprocessor
 	{
 		val _code = new StringBuilder()
 
-		val codeLines = InferSemicolonsPreprocessingCommand(ReplaceTabsToBeginAndEndPreprocessingCommand(code.split("\n").map(new CodeLine(_))))
+		val codeLines = InferSemicolonsPreprocessingCommand(CodeBlockInference(code.split("\n").map(new CodeLine(_))))
 
 		codeLines.foreach(cl =>
 		{
